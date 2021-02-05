7ba2b3e3d564de34dd0f77accdaa6485a32d3e8d
Harish Mahadi <dravindr@stud.hs-heilbronn.de>
2021-02-05 15:36:35 +0100

    code refactor in data.service

--- src/services/data.service.ts
+++ src/services/data.service.ts
@@ -3,7 +3,6 @@ import {Http} from '@angular/http';
 import {Observable} from 'rxjs';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
-import 'rxjs/add/operator/map';
 import {environment} from '../environments/environment';
 
 @Injectable({
@@ -15,37 +14,21 @@ export class DataService {
 
   getPokeApiList(limit: number, offset: number) {
     let _url: string = `${environment['baseUrl']}?limit=${limit}&offset=${offset}/`;
-    return this.http.get(_url).map(
-      res => {
-        return res;
-      })
-      .catch(this._errorHandler);
+    return this.http.get(_url).catch(this._errorHandler);
   };
 
   getPokeApiDetail(name: string) {
     let _url: string = `${environment['baseUrl']}/${name}`;
-    return this.http.get(_url).map(
-      res => {
-        return res;
-      })
-      .catch(this._errorHandler);
+    return this.http.get(_url).catch(this._errorHandler);
   };
 
   getPokemons(id: number) {
     let _url: string = `${environment['baseUrl']}/${id}`;
-    return this.http.get(_url).map(
-      res => {
-        return res;
-      })
-      .catch(this._errorHandler);
+    return this.http.get(_url).catch(this._errorHandler);
   };
 
   getPokeMovesStats(url: string) {
-    return this.http.get(url).map(
-      res => {
-        return res;
-      })
-      .catch(this._errorHandler);
+    return this.http.get(url).catch(this._errorHandler);
   };
 
   private _errorHandler(error: Response) {
